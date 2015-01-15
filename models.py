65238cdee60e0392c83dd334ba8b188d1309147b
unknown <plynp4@POSNT01.ad.nottingham.ac.uk>
2015-01-15 18:06:38 +0000

    Created Content class

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Content (models.Model):
+	content = models.CharField(max_length=140)
+	content_type = models.CharField(max_length=6)
+	duration = models.IntegerField(min_value=1)
+	start = models.DateTimeField()
+	end = models.DateTimeField()
+	rank = models.IntegerField(min_value=1)
+	#feed = models.ForeignKey()
+	#user = models.ForeignKey()
+	
