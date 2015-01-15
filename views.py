a083f433f297d62fd6f10ff7a1ed3fa64972ecc9
Tom Price <tom@codedinternet.com>
2015-01-15 18:43:12 +0000

    Made changes to login and template

--- views.py
+++ views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 from django.views import generic
 
 # Create your views here.
 class Index(generic.TemplateView):
     template_name = 'signage/index.html'
+
+# Custom logon class to redirect to the previous page
+def login(request, **kwargs):
+    if request.user.is_authenticated():
+        next = request.REQUEST.get('next', '/')
+        return HttpResponseRedirect(request.REQUEST.get('next', '/'))
+    else:
+        from django.contrib.auth.views import login
+
+        return login(request)
