552a940bfe958358455654913cfa36c8fd0374cf
unknown <plynp4@POSP01830.ad.nottingham.ac.uk>
2015-01-28 16:57:34 +0000

    Added ForiegnKeys and ManyToManyFields

--- models.py
+++ models.py
@@ -1,23 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import Group
+from django.conf import settings
 
 # Create your models here.
-class Content (models.Model):
-	content = models.CharField(max_length=140)
-	content_type = models.CharField(max_length=6)
-	duration = models.IntegerField(min_value=1)
-	start = models.DateTimeField()
-	end = models.DateTimeField()
-	rank = models.IntegerField(min_value=1)
-	#feed = models.ForeignKey()
-	#user = models.ForeignKey()
-	
-class Feed (models.Model):
-	feedname = models.CharField(max_length=80)
-	
-class Screen (models.Model):
-	screenname = models.CharField(max_length=80)
-	hash = models.CharField(max_length=32)
-	
-class ScreenFeed (models.Model):
-	#screenfeed = 
-	
+class Content(models.Model):
+    content = models.CharField(max_length=140)
+    content_type = models.CharField(max_length=6)
+    duration = models.IntegerField(min_value=1)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    rank = models.IntegerField(min_value=1)
+    feed = models.ForeignKey(Feed)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+class Feed(models.Model):
+    feedname = models.CharField(max_length=80)
+    group = models.ForeignKey(Group)
+
+
+class Screen(models.Model):
+    screenname = models.CharField(max_length=80)
+    hash = models.CharField(max_length=32)
+    feeds = models.ManyToManyField(Feed)
